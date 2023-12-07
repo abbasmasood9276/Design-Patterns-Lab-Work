b757cffc8abf91bf3ef0f842a3e5222829f68086
abbasmasood9276 <145642179+abbasmasood9276@users.noreply.github.com>
2023-12-07 10:29:14 +0500

    updated

---
+++ Design Patterns Lab/src/State/ReadyState.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package State;
+
+/**
+ *
+ * @author fa20-bse-001
+ */
+
+/**
+ * They can also trigger state transitions in the context.
+ */
+public class ReadyState extends State {
+
+    public ReadyState(Player player) {
+        super(player);
+    }
+
+    @Override
+    public String onLock() {
+        player.changeState(new LockedState(player));
+        return "Locked...";
+    }
+
+    @Override
+    public String onPlay() {
+        String action = player.startPlayback();
+        player.changeState(new PlayingState(player));
+        return action;
+    }
+
+    @Override
+    public String onNext() {
+        return "Locked...";
+    }
+
+    @Override
+    public String onPrevious() {
+        return "Locked...";
+    }
+}
+
